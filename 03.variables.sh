b699d0cbc180c627f570127c79fc4b0412cc9883
manu <example@gmail.com>
2026-01-28 16:57:38 +0530

    changes made

---
+++ 03.variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=suresh
+PERSON2=ramesh
+
+echo "$PERSON1:: Hello $PERSON2, How are you?"
+echo "$PERSON2:: Hi $PERSON1, I am fine. How are you"
+echo "$PERSON1:: I am Fine. what are you learning $PERSON2?"
+echo "$PERSON2:: I am learning Shell scripting. what about you?"
+
