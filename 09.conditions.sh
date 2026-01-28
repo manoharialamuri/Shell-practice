df353e3054b5578a0142087f7daa4bf5d329cd76
manu <example@gmail.com>
2026-01-28 18:38:01 +0530

    changes made

--- 09.conditions.sh
+++ 09.conditions.sh
@@ -4,7 +4,7 @@ NUM=$1
 
 if [ $NUM -gt 20 ]; then
     echo "Given number : $NUM is greater than 20"
-elif [ $NUM -eq 20]; then
+elif [ $NUM -eq 20 ]; then
     echo "Given Number is: $NUM is equal to 20"
 else
     echo "Given number : $NUM is lesser than 20"
