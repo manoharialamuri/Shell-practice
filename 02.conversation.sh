7435ff0c7921233e4abc6b7a4bb8f5f65749f557
manu <example@gmail.com>
2026-01-28 16:48:38 +0530

    changes made

---
+++ 02.conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "suresh:: Hello Ramesh, How are you?"
+echo "Ramesh:: Hi Suresh, I am fine. How are you"
+echo "suresh:: I am Fine. what are you learning ramesh?"
+echo "Ramesh:: I am learning Shell scripting. what about you?"
+
