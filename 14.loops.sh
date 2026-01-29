d93a4fb6f48dd988b1360149c97903e2614f05b3
manu <example@gmail.com>
2026-01-29 22:41:58 +0530

    actions

---
+++ 14.loops.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/$0.log"
+
+
+if [ $USERID -ne 0 ]; then
+    echo "Please use root access" | tee -a $LOGS_FILE
+    exit 12
+fi
+
+validate (){
+    if [ $1 -ne 0 ]; then
+        echo "$2... Failed"
+        exit 30
+    else
+        echo "$2.. Success"
+    fi
+}
+
+for package $@
+do
+    echo "dnf install $package -y"
+    validate "Installing $package"
+done
