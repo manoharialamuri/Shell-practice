6fa86cc0c08444dcd2b11867397ec406dd6c1855
manu <example@gmail.com>
2026-01-29 17:23:57 +0530

    actions

--- 10.installation.sh
+++ 10.installation.sh
@@ -17,4 +17,20 @@ else
     echo "Installing nginx.. Success"
 fi
 
+dnf install mysql -y
 
+if [ $? -ne 0 ]; then
+    echo "Installing mysql.....Failed"
+    exit 2
+else   
+    echo "Installing mysql....Success"
+fi
+
+dnf install nodejs -y
+
+if [ $? -ne 0 ];then
+    echo "Installing nodejs ...Failed"
+    exit 3
+else
+    echo"Installing nodeja....Success"
+fi
