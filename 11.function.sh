ee9393ca408bf554da6fc9ae5364a7e68a86b3fe
manu <example@gmail.com>
2026-01-29 18:53:40 +0530

    actions

---
+++ 11.function.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "Please use root access"
+    exit 12
+fi
+
+validate (){
+    if [ $1 -ne 0 ]; then
+        echo "$2... Failed"
+        exit 30
+    else
+        echo "$2.. Success"
+    fi
+}
+
+dnf install nginx -y
+validate $? "Installing nginx"
+
+dnf install mysql -y
+validate $? "Installing mysql"
+
+dnf install nodejs -y
+validate $? "Installing nodejs"
